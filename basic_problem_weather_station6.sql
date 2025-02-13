ffefbd49502e915ac8601030db6d514f6aa1b062
Vidnyani Umathe <vidnyani.umathe@gmail.com>
2025-02-13 21:56:37 +0530

    Added weather problem station 6

---
+++ basic_problem_weather_station6.sql
@@ -0,0 +1,2 @@
+-- Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
+SELECT DISTINCT CITY FROM STATION WHERE lower(substr(CITY,1,1)) in ('a','e','i','o','u') ;
