52cc55f9112a701db2dd17713240eac71a8573d9
Vidnyani Umathe <vidnyani.umathe@gmail.com>
2025-03-28 21:39:09 +0530

    updated sql project planning

--- sql_project_planning.sql
+++ sql_project_planning.sql
@@ -1,5 +1,6 @@
 
--- Write a query to output the start and end dates of projects listed by the number of days it took to complete the project in ascending order. If there is more than one project that have the same number of completion days, then order by the start date of the project.
+-- Write a query to output the start and end dates of projects listed by the number of days it took to complete the project in ascending order. If there is more than one project that have the same number of completion days, 
+-- then order by the start date of the project.
 select a.start_date, min(b.end_date)
 from
 (select p1.start_date 
