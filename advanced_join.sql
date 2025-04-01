8d84cd08f8f7914ecf7067e6f57135b900d966cd
Vidnyani Umathe <vidnyani.umathe@gmail.com>
2025-04-01 07:48:04 +0530

    Added advanced join problem

---
+++ advanced_join.sql
@@ -0,0 +1,13 @@
+
+SELECT DISTINCT f1.x,f1.y 
+FROM   functions f1 
+       inner join functions f2 
+               ON f1.x IN f2.y 
+                  AND f1.y IN f2.x 
+WHERE  f1.x < f1.y 
+        OR f1.x IN (SELECT x 
+                   FROM   functions 
+                   WHERE  x = y 
+                   GROUP  BY x,y 
+                   HAVING Count(*) > 1) 
+ORDER  BY f1.x; 
